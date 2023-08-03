964a7155de6be02526f945a4f274099b59a64bdd
Dimas Yudha Pratama <official@dimasyudha.tech>
2023-08-03 20:11:27 +0700

    sirius: Enforce the SELinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,8 +34,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
-                        androidboot.usbcontroller=a600000.dwc3 \
-                        androidboot.selinux=permissive
+                        androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 \
                         msm_rtb.filter=0x237 \
                         ehci-hcd.park=3 \
